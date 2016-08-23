40b289138125ea2838a3bd8c469cda2cb3569791
spyderspinner <spyderricerichson@gmail.com>
2016-08-22 18:48:58 -0700

    removes commented code

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
-<!-- app.get('/', index.html);
- -->
-<!-- exports.index = function(req, res){
-  res.sendfile('index.html');
-};
- -->
 module.exports = function (app) {
 
   app.get('/', function (req, res, next) {
     res.render('index')
   });
 
-}
+};
 
 
 console.log("Hello Spyder!!")
