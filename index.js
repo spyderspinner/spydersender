b607a45da3527346694549fb3dad9240df6d6544
spyderspinner <spyderricerichson@gmail.com>
2016-08-22 19:12:09 -0700

    sendsfile instead of render

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var express = require('express')
+  , app = express();
   , http = require('http')
   , path = require('path');
-
-var app = express();
 // require('./index')(app)
 
 // module.exports = function (app) {
 
-  app.get('/', function (req, res, next) {
-    res.render('index.html')
-  });
+  // app.get('/', function (req, res, next) {
+  //   res.render('index.html')
+  // });
 
 // };
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 
 console.log("Hello Spyder!!")
