17636ef2a8fce497dd0f31dfb84ec266ba3deb80
spyderspinner <spyderricerichson@gmail.com>
2016-08-22 18:59:45 -0700

    disables appjs

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
+var app = express();
+require('./routes/index')(app)
+
 module.exports = function (app) {
 
   app.get('/', function (req, res, next) {
