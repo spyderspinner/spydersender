d571e92ebae661d2f2599004851326dd1aaa99e2
spyderspinner <spyderricerichson@gmail.com>
2016-08-22 22:00:29 -0700

    connects view

--- index.js
+++ index.js
@@ -6,10 +6,44 @@ var express = require('express')
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('See me here!');
-});
+app.set('title', 'SpinnerSender');
+app.set('views', __dirname + '/views'); // general config
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-app.listen(process.env.PORT, function () {
+
+app.listen(3000, function () {
   console.log('Listening on port!');
 });
+
+// app.configure(function(){
+//    app.set('views', __dirname + '/views');
+//    app.set('views');
+//    // => "/absolute/path/to/views"
+
+//    // app.enable('some feature');
+//    // same as app.set('some feature', true);
+
+//    // app.disable('some feature');
+//    // same as app.set('some feature', false);
+
+//    // app.enabled('some feature')
+//    // => false
+
+//    app.configure(function(){
+//        app.use(express.methodOverride());
+//        app.use(express.bodyParser());
+//        app.use(app.router);
+//    });
+
+// });
+
+function checkInfo() {
+};
+
+app.get('/', function (req, res) {
+  // res.send('See me here!');
+  res.render('index')
+  // buildIntakeForm;
+});
+
