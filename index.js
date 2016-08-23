7fbadc37728d3523d38033f2af98e12053d4ec1a
spyderspinner <spyderricerichson@gmail.com>
2016-08-22 19:48:38 -0700

    generalizes port

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/', function (req, res) {
   res.send('See me here!');
 });
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+app.listen(process.env.PORT, function () {
+  console.log('Listening on port!');
 });
